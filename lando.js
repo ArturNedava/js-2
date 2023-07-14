e910370235424e40134c42b3e9c0bf1d79fdca96
ArturNedava <121451856+ArturNedava@users.noreply.github.com>
2023-07-14 17:42:03 +0200

    First commit

---
+++ lando.js
@@ -0,0 +1,83 @@
+//let celcius = 15;
+
+//let farenheit = (celcius * 9/5) +32;
+
+//console.log(farenheit);
+
+
+//let daysInMonth = 31;
+
+//let hoursInMonth = daysInMonth * 24;
+
+//let minutesInMonth = hoursInMonth * 60;
+
+//console.log("Количество часов в месяце:", hoursInMonth);
+//console.log("Количество минут в месяце:", minutesInMonth);
+
+
+//let health = 100;
+
+//let energy = 50;
+
+//health -=50;
+
+//energy -=25;
+
+//console.log("Уровень здоровья",health)
+
+//console.log("Уровень енергии",energy)
+
+
+//let total = 1500;
+
+//let discont = total * 10 / 100;
+
+//console.log("To Pay:", + total-discont);
+
+
+//let floatingNumber = 5.14;
+
+//let roundedNumber = Math.floor(floatingNumber);
+
+//console.log("Округлене число:", roundedNumber);
+
+
+
+//let numberString = "3.14";
+
+//let Number = parseFloat(numberString);
+
+//console.log("Перетворене число:", parsedNumber);
+
+
+
+//let str = "12345";
+
+//let num = parseInt(str);
+
+//console.log(num);
+
+
+//let number = 16;
+
+//let MathNumber = Math.sqrt(number);
+
+//console.log(MathNumber);
+
+
+//let numberString = 45;
+
+//console.log("начальный рядок"+ numberString);
+
+//let number = parseInt(numberString);
+
+//console.log("Целое число: " + number);
+
+//let string = number.toString();
+
+//console.log("Переделаный рядок: " + string);
+
+
+
+
+
